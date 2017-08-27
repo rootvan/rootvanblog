0f2d08b83d9eb28300b2945a37f9a832af620c9e
rootvan <ridvan.karadag@std.iszu.edu.tr>
2017-08-28 02:45:37 +0300

    Görünüm sayfasında kategori adının listelenmesi sağlandı

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,6 +7,8 @@
             <div class="col-lg-8 col-lg-offset-2">
                 <h2 class="page-header">{{$post->title}}</h2>
                 <p>{{$post->content}}</p>
+                <hr>
+                <p>{{$category->category_name}}</p>
             </div>
         </div>
     </div>
