8d311c0d6521f7be8a1119858b7436f31972efe4
rootvan <ridvan.karadag@std.iszu.edu.tr>
2017-08-28 01:51:49 +0300

    Postların yeniden eskiye sıralanması sağlandı

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('id','desc')->get();
         return view('home')->with('posts',$posts);
     }
 
