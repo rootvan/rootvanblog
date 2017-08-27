1a82a95727576982f5a96814a74c45f3344fc086
rootvan <ridvan.karadag@std.iszu.edu.tr>
2017-08-28 01:59:32 +0300

    Başlığın yanına tarih bilgisi eklendi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
         <div class="col-lg-8 col-lg-offset-2">
             @foreach($posts as $post)
-                <h2><a href="posts/{{$post->id}}">{{$post->title}}</a></h2>
+                <h2><a href="posts/{{$post->id}}">{{$post->title}}</a> <small>{{$post->created_at->diffForHumans()}}</small></h2>
                 @endforeach
         </div>
     </div>
